6d0b096493e25e57c6b8de0e524ffc9b163e78e8
Forb Yuan <f0rb@163.com>
2022-11-26 23:01:03 +0800

    [doyto-query-mongodb] add SPI implementation for DataAccessFactory

--- doyto-query-mongodb/src/test/java/win/doyto/query/mongodb/MongoDataAccessFactoryTest.java
+++ doyto-query-mongodb/src/test/java/win/doyto/query/mongodb/MongoDataAccessFactoryTest.java
@@ -16,21 +16,16 @@
 
 package win.doyto.query.mongodb;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import win.doyto.query.core.DataAccess;
 import win.doyto.query.core.DoytoQuery;
-import win.doyto.query.mongodb.session.MongoSessionSupplier;
 import win.doyto.query.mongodb.test.inventory.InventoryEntity;
 
+import javax.annotation.Resource;
 import javax.persistence.EntityType;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 /**
  * MongoDataAccessFactoryTest
@@ -38,17 +33,17 @@ import static org.mockito.Mockito.when;
  * @author f0rb on 2022/11/26
  * @since 1.0.0
  */
-class MongoDataAccessFactoryTest {
+class MongoDataAccessFactoryTest extends MongoApplicationTest {
+
+    @Resource
+    private BeanFactory beanFactory;
+
     @Test
     void createDataAccessByMongoClient() {
         MongoDataAccessFactory factory = new MongoDataAccessFactory();
-        BeanFactory mockBeanFactory = mock(BeanFactory.class);
-        when(mockBeanFactory.getBean(MongoSessionSupplier.class))
-                .thenThrow(new NoSuchBeanDefinitionException(MongoSessionSupplier.class));
-        when(mockBeanFactory.getBean(MongoClient.class)).thenReturn(MongoClients.create());
 
         DataAccess<InventoryEntity, String, DoytoQuery> dataAccess =
-                factory.createDataAccess(mockBeanFactory, InventoryEntity.class);
+                factory.createDataAccess(beanFactory, InventoryEntity.class);
 
         assertThat(factory.getEntityType()).isEqualTo(EntityType.MONGO_DB);
         assertThat(dataAccess).isInstanceOf(MongoDataAccess.class);
